afc1bfe37571f515c346bef33c497f2198831a97
salah-jr <mohammed.s.korna@gmail.com>
2023-01-27 16:23:46 -0800

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"github.com/gocolly/colly"
+	"log"
+	"os"
+)
+
+func main() {
+	fName := "data.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Could not create file, err: %q", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	c := colly.NewCollector()
+	c.OnHTML("#postsList", func(e *colly.HTMLElement) {
+		e.ForEach("div", func(_ int, el *colly.HTMLElement) {
+			writer.Write([]string{
+				el.ChildText("h2"),
+			})
+		})
+		fmt.Println("Scrapping Complete")
+	})
+	c.Visit("https://haraj.com.sa/search/مطلوب%20شريك?duringdate=1months")
+}
